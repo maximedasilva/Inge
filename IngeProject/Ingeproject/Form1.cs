a765272d122b73f6f14d2347307962ddad08e652
maximedasilva <maximedasilva40@gmail.com>
2016-10-24 19:57:33 +0200

    first commit

---
+++ IngeProject/Ingeproject/Form1.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Ingeproject
+{
+    public partial class Form1 : Form
+    {
+        int i = 1;
+        Stopwatch t = new Stopwatch();
+        int[,] tab = { 
+            { 20, 179 }, 
+            { 160, 147 }, 
+            { 100, 58 }, 
+            { 400, 170 }, 
+            { 270, 27 }, 
+            { 150, 290 }, 
+            { 100, 100 }, 
+            { 480, 380 },
+            { 40, 280 }, 
+            { 280, 20 } };
+
+
+        public Form1()
+        {
+            InitializeComponent();
+            this.Width = 550;
+            this.Height = 550;
+            Inverter v = new Inverter(false, false);
+            Button b = new Button();
+            b.Top = 100;
+            b.Left = 50;
+            b.Click += buttonClick;
+            v.Start();
+            this.Controls.Add(b);
+        }
+        public void buttonClick(Object sender,EventArgs e)
+        {
+            if(i==1)
+            {
+                t.Start();
+            }
+            //MessageBox.Show("coucou");
+            Button old = (Button)sender;
+            old.Visible = false;
+
+            if (i < tab.GetLength(0))
+            {
+                Button b = new Button();
+                b.Click += buttonClick;
+                b.Left = tab[i, 0];
+                b.Top = tab[i, 1];
+                this.Controls.Add(b);
+                i++;
+            }
+            else
+            {
+                t.Stop();
+                MessageBox.Show(Convert.ToString(t.Elapsed.Seconds)+' '+Convert.ToString(t.Elapsed.Milliseconds));
+            }
+            
+        }
+    }
+}
---
+++ IngeProject/Ingeproject/Inverter.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+using System.Windows.Forms;
+using System.Threading;
+
+namespace Ingeproject
+{
+    internal class Inverter
+    {
+        private Point pos = Cursor.Position;
+
+        private bool invertX;
+
+        private bool invertY;
+
+        private bool running;
+
+        private bool exit;
+
+        public bool InvertX
+        {
+            get
+            {
+                return this.invertX;
+            }
+            set
+            {
+                this.invertX = value;
+                if (this.InvertSettingsChanged != null)
+                {
+                    this.InvertSettingsChanged(this, new EventArgs());
+                }
+            }
+        }
+
+        public bool InvertY
+        {
+            get
+            {
+                return this.invertY;
+            }
+            set
+            {
+                this.invertY = value;
+                if (this.InvertSettingsChanged != null)
+                {
+                    this.InvertSettingsChanged(this, new EventArgs());
+                }
+            }
+        }
+
+        public bool Running
+        {
+            get
+            {
+                return this.running;
+            }
+        }
+
+        public Inverter(bool x, bool y)
+        {
+            this.invertX = x;
+            this.invertY = y;
+            this.pos = Cursor.Position;
+        }
+
+        private void MouseLoop()
+        {
+            Thread.CurrentThread.IsBackground = true;
+            Thread.CurrentThread.Priority = ThreadPriority.Highest;
+            while (!this.exit)
+            {
+                Point position = Cursor.Position;
+                int right = (this.invertX ? this.pos.X - (position.X - this.pos.X) : position.X);
+                if (this.invertX)
+                {
+                    if (right < 2)
+                    {
+                        right = 2;
+                    }
+                    if (right > Screen.FromPoint(position).Bounds.Right - 2)
+                    {
+                        Rectangle bounds = Screen.FromPoint(position).Bounds;
+                        right = bounds.Right - 2;
+                    }
+                }
+                int bottom = (this.invertY ? this.pos.Y - (position.Y - this.pos.Y) : position.Y);
+                if (this.invertY)
+                {
+                    if (bottom < 2)
+                    {
+                        bottom = 2;
+                    }
+                    if (bottom > Screen.FromPoint(position).Bounds.Bottom - 2)
+                    {
+                        Rectangle rectangle = Screen.FromPoint(position).Bounds;
+                        bottom = rectangle.Bottom - 2;
+                    }
+                }
+                Cursor.Position = new Point(right, bottom);
+                this.pos = Cursor.Position;
+                Thread.Sleep(1);
+            }
+            this.exit = false;
+        }
+
+        public void Start()
+        {
+            this.pos = Cursor.Position;
+            this.running = true;
+            (new Thread(new ThreadStart(this.MouseLoop))).Start();
+        }
+
+        public void Stop()
+        {
+            this.running = false;
+            this.exit = true;
+        }
+
+        public event EventHandler InvertSettingsChanged;
+    }
+}
