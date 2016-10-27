333916ce5b74e3bdc04661a3b45ee00301b0b8b1
maximedasilva <maximedasilva40@gmail.com>
2016-10-27 14:56:52 +0200

    add xmlSaver

---
+++ IngeProject/XMLSaver/Class1.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+using System.Xml.Linq;
+namespace XMLSaver
+{
+    public class XMLSaver
+    {
+        public string file { get;private set; }
+        public XDocument doc { get; private set; }
+        public XMLSaver()
+        {
+            create();
+        }
+        public XMLSaver(string path)
+        {
+            file = path;
+            
+        }
+        public void create()
+        {
+            string current = System.IO.Directory.GetCurrentDirectory();
+            string fileName = System.IO.Path.GetRandomFileName();
+            string pathTotal= System.IO.Path.Combine(current, fileName);
+            doc = XDocument.Load(pathTotal);
+        }
+        public void create(string path)
+        {
+            try
+            {
+                System.IO.File.Create(path);
+                doc = XDocument.Load(path);
+            }
+            catch (Exception e)
+            {
+
+                throw;
+            }
+        }
+
+    }
+}
